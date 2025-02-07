ebd4e19635271a5999f03c9269e8163e7cca6831
ichi1007 <mail@ichi10.com>
2025-02-08 01:15:23 +0900

    DocsPageコンポーネントを追加し、Headerをインポート; 不要なNext.js設定ファイルを削除

---
+++ app/docs/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/component/home/HomeHeader"
+
+export default function DocsPage() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-center font-bold text-5xl my-10">DockPage</h1>
+    </div>
+  );
+};
