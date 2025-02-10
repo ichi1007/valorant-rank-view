41394a25cf1e47b1fcd12c8933db16c786af091d
ichi1007 <mail@ichi10.com>
2025-02-11 00:14:17 +0900

    ウェイティングリストページのレイアウトを改善し、ユーザーへの案内文を追加; メールアドレス入力の説明を強化

--- app/(oauth)/waitlist/[[...waitlist]]/page.tsx
+++ app/(oauth)/waitlist/[[...waitlist]]/page.tsx
@@ -11,8 +11,28 @@ export default function SignInPage() {
   return (
     <>
       <Header />
-      <div className="flex items-center justify-center h-[70vh]">
-        <Waitlist />
+      <div
+        id="waitlist"
+        className="flex justify-center items-center flex-wrap mb-10 h-[90vh]"
+      >
+        <div className="max-w-[540px]">
+          <h1 className="text-center text-4xl font-bold mb-4">
+            ウェイティングリストに登録する
+          </h1>
+          <p className="mx-2">
+            メールアドレスを入力するだけでアプリを使用する権限を申請することができます。
+          </p>
+          <p className="mx-2 mt-1 text-xs">
+            Gmailの方は
+            <span className="font-bold">&quot;プロモーション&quot;</span>
+            の受信箱に招待状が送られます。
+            <br />
+            お手数をおかけいたしますが、連絡が遅い場合は一度ご確認ください。
+          </p>
+        </div>
+        <div className="flex justify-center my-5 mx-5">
+          <Waitlist />
+        </div>
       </div>
     </>
   );
--- component/home/waitlist.tsx
+++ component/home/waitlist.tsx
@@ -13,6 +13,13 @@ export default function WaitlistSec() {
         <p className="mx-2">
           メールアドレスを入力するだけでアプリを使用する権限を申請することができます。
         </p>
+        <p className="mx-2 mt-1 text-xs">
+          Gmailの方は
+          <span className="font-bold">&quot;プロモーション&quot;</span>
+          の受信箱に招待状が送られます。
+          <br />
+          お手数をおかけいたしますが、連絡が遅い場合は一度ご確認ください。
+        </p>
       </div>
       <div className="flex justify-center my-5 mx-5">
         <Waitlist />
