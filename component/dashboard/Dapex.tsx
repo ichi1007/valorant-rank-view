932785dc03b25296a7172363055a15e4a1d788c8
ichi1007 <mail@ichi10.com>
2025-02-11 00:47:01 +0900

    Dapexコンポーネントに「Coming soon...」のメッセージを追加

--- component/dashboard/Dapex.tsx
+++ component/dashboard/Dapex.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export const Dapex = () => {
   return (
     <div className="w-full h-full">
-      <div className="w-full h-full bg-gray-200 dark:bg-neutral-700 rounded-lg" />
+      <div className="w-full h-full bg-gray-200 dark:bg-neutral-700 rounded-lg">
+        <h1 className="h-[100%] text-3xl font-bold flex items-center justify-center">Coming soon...</h1>
+      </div>
     </div>
   );
 };
