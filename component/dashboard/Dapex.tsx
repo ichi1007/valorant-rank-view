0a1a902ea6a9b27169b1ec9e47bbb0a03b08890c
ichi1007 <mail@ichi10.com>
2025-02-11 00:48:46 +0900

    Dapexコンポーネントの「Coming soon...」メッセージにダークモード用のテキスト色を追加

--- component/dashboard/Dapex.tsx
+++ component/dashboard/Dapex.tsx
@@ -4,7 +4,7 @@ export const Dapex = () => {
   return (
     <div className="w-full h-full">
       <div className="w-full h-full bg-gray-200 dark:bg-neutral-700 rounded-lg">
-        <h1 className="h-[100%] text-3xl font-bold flex items-center justify-center">Coming soon...</h1>
+        <h1 className="h-[100%] text-3xl font-bold flex items-center justify-center dark:text-white">Coming soon...</h1>
       </div>
     </div>
   );
