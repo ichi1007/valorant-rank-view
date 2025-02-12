7327f0b7d95b600cfcbbf100314d6fbfc26a7600
ichi1007 <mail@ichi10.com>
2025-02-13 02:25:42 +0900

    フォントを変更し、フッターに著作権情報を追加

--- component/footer.tsx
+++ component/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div id="footer">
       <div className="bg-gray-100 border-t px-5 py-8">
-        <ul className="w-full max-w-[1000px] mx-auto text-xs px-4">
+        <ul className="w-full max-w-[1000px] mx-auto text-xs px-4 font-[200]">
           <li>
             VALORANT及びライアットゲームズは、
             <Link
@@ -38,12 +38,30 @@ export default function Footer() {
             </Link>
             , Inc. が公式に提供しているものを使用しております。
           </li>
+          <li>
+            本サイトでは、&copy;{" "}
+            <Link href="https://line.me" className="underline" target="_blank">
+              LY Corporation
+            </Link>
+            , が提供する{" "}
+            <Link
+              href="https://seed.line.me/"
+              className="underline"
+              target="_blank"
+            >
+              Line Seed Font
+            </Link>{" "}
+            を使用しています。
+          </li>
         </ul>
       </div>
       <footer className="w-full bg-white border-t text-black text-center py-5 text-sm">
         <p>
           © {new Date().getFullYear()}{" "}
-          <Link href="https://ichi10.com" target="_blank">ichi</Link>, Dev.
+          <Link href="https://ichi10.com" target="_blank">
+            ichi
+          </Link>
+          , Dev.
         </p>
       </footer>
     </div>
