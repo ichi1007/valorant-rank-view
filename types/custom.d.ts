1010468d067892d128016c06e5e3bbee8deee9cc
ichi1007 <mail@ichi10.com>
2025-03-16 04:36:29 +0900

    IntrinsicElementsをinterfaceからtypeに変更し、"equivalent to supertype"エラーを回避

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -2,6 +2,7 @@ import React from "react";
 
 declare global {
   namespace JSX {
-    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
+    // Using type instead of interface to avoid the "equivalent to supertype" error
+    type IntrinsicElements = React.JSX.IntrinsicElements;
   }
 }
